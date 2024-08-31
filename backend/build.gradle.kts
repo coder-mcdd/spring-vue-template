0c2e0c76cd8fc38cbf967dfa7f8ffd1d6718bb6f
coder-mcdd <mcdd1024@gmail.com>
2024-08-31 13:40:43 +0800

    :bricks: build: add mail、jwt、swagger-ui、configuration-processor、amqp dep

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "jzxy.mcdd"
-version = "0.0.1-SNAPSHOT"
+version = "1.0.0"
 
 java {
     toolchain {
@@ -24,13 +24,17 @@ repositories {
 }
 
 dependencies {
-    // https://mvnrepository.com/artifact/com.alibaba.fastjson2/fastjson2
+    implementation("com.auth0:java-jwt:4.3.0")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
     implementation("com.alibaba.fastjson2:fastjson2:2.0.20")
     implementation ("com.baomidou:mybatis-plus-spring-boot3-starter:3.5.7")
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-amqp")
     implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-mail")
+    implementation("org.springframework.boot:spring-boot-configuration-processor")
     testImplementation("org.springframework.security:spring-security-test")
     compileOnly("org.projectlombok:lombok")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
